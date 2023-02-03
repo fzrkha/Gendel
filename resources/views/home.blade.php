b378581c7e7257f84b61f6e8e23daf963ec8f5bf
fzrkha <redfanz02@gmail.com>
2023-02-03 10:01:51 +0700

    Menambah file home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+</head>
+<body>
+    <h1 algin="center">Home Page</h1>
+    <h3>yyy</h3>
+</body>
+</html>
